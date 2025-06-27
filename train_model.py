1fe04ca2841203644c952d657bab9b8b1aefe537
Eswaran Muthu <38020741+EswaranMuthu@users.noreply.github.com>
2025-06-27 16:47:49 -0400

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
 
 # Replace with the actual filename you see
 df = pd.read_csv("/kaggle/input/bitcoin-historical-data/btcusd_1-min_data.csv")
@@ -20,9 +23,6 @@ X = df[features]
 y = df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
-
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, classification_report
 
 model = XGBClassifier(missing=np.inf)
 model.fit(X_train, y_train)
